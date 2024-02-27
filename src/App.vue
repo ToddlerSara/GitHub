a8c8663f52a7fd2d6fc57a6a1d2b89f8a2836412
ToddlerSara <160302245+ToddlerSara@users.noreply.github.com>
2024-02-27 17:24:27 +0800

    html 切版練習
    html 切版練習

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,53 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import Iframe from './components/Iframe.vue'
 const msg1 = ref(null);
+const clas = 'logo'
+const articleList = ref([
+  { name: 'article A', content: '測試A' },
+  { name: 'article B', content: '測試B' },
+  { name: 'article C', content: '測試C' },
+])
 
-onMounted(()=>{
-  msg1.value = 'Hey you';
-})
+//ctrl+KC為註解，ctrl+KU取消註解
+// onMounted(()=>{
+//   msg1.value = 'Hey you';
+// })
 
 </script>
 
 <template>
+  <div class="body">
+    <div class="header">
+      <div class="name">left</div>
+      <div class="menu">
+        <ul>
+          <li class="formstytle">Hello</li>
+          <li class="formstytle">About me</li>
+          <li class="formstytle">Practice</li>
+        </ul>
+      </div>
+    </div>
+    <div class="banner">
+      <div class="context-up">
+        <p>測試</p>
+      </div>
+    </div>
+    <div class="articleArea">
+      <div v-for="(article, idx) in articleList">
+        <h2>{{ article.name }}</h2>
+        <p>{{ article.content }}</p>
+      </div>
+    </div>
+    <div class="footerArea">
+      footer
+    </div>
+  </div>
+
+
+  <!-- 
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
+    <img alt="Vue logo" 
+     src="./assets/logo.svg" width="125" height="125"/>
     <div class="wrapper">
       <HelloWorld :msg="msg1" :show="false"/>
       <p>This<i> is </i>some content HTML <br><strong>練習測試</strong></p>
@@ -23,7 +60,7 @@ onMounted(()=>{
         <Iframe src="https://www.youtube.com/embed/LdOUKrbVc0I?si=qXhqrSeWNkDa_XeR" />
       </template>
       <ul>
-        <li v-for="i in 10">List {{ i }}</li>
+        <li v-for="(i, idx) in 10">List {{ i }}</li>
       </ul><br>
       <ol>
         <li>List 1</li>
@@ -54,10 +91,52 @@ onMounted(()=>{
       <br>
       <textarea></textarea>
     </form>
-  </main>
+  </main> -->
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.body {
+  >* {
+    border: 1px solid #000;
+    margin: 5px;
+    padding: 10px;
+  }
+
+  .formstytle {
+    display: inline-block;
+    margin: 5px;
+    padding: 5px;
+  }
+
+  //新增背景圖
+  .banner {
+    block-size: 150px;
+    writing-mode: horizontal-tb;
+    background-image: url("https://i.imgur.com/W7xqcWS.jpeg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  //調整文字在上
+  .context-up {
+    position: absolute;
+    color: #8a0707;
+    font-size: 36px;
+    margin: 0 auto;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+
 header {
   line-height: 1.5;
 }
@@ -67,6 +146,7 @@ header {
   margin: 0 auto 2rem;
 }
 
+//RWD 1024px up
 @media (min-width: 1024px) {
   header {
     display: flex;
@@ -78,7 +158,7 @@ header {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
+  header .wrapper .content .header .right {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
